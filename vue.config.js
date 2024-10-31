28edc31d0e0e566f9ab5c8fcd17fcde96f13f162
gstio86 <bailung20000218@gmail.com>
2024-10-31 16:34:25 +0800

    更新vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/ShortURL/' : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/ShortURL/' : '/',
+    chainWebpack: config => {
+        config.plugin('copy').tap(([options]) => {
+            options[0].ignore.push('404.html');
+            return [options];
+        });
+    },
+    configureWebpack: {
+        plugins: [
+            {
+                apply: (compiler) => {
+                    compiler.hooks.done.tap('Copy404Plugin', () => {
+                        fs.copyFileSync('dist/index.html', 'dist/404.html');
+                    });
+                }
+            }
+        ]
+    }
 };
