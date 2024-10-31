53f797f4145c2ec49c685e0367fda697e3c6ab6d
gstio86 <bailung20000218@gmail.com>
2024-10-31 20:20:04 +0800

    更新router排除後端路徑

--- src/router/router.js
+++ src/router/router.js
@@ -3,6 +3,8 @@ import Home from '@/views/Home.vue';
 
 const routes = [
     { path: '/', component: Home },
+    { path: '/api/:pathMatch(.*)*', component: () => null },
+    { path: '/:url([a-zA-Z0-9]{7})', component: () => null },
     { path: '/:any(.*)*', redirect: '/' }
 ]
 
